44f06044bef6c709f0ca73d4f6e16bffaf57e642
Fepozopo <fepozopo.f39@gmail.com>
2025-02-05 19:44:11 -0800

    Updated main.go to handle too little or too many CLI argements

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	if len(os.Args) < 2 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	} else if len(os.Args) > 2 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	} else {
+		fmt.Printf("starting crawl of: %s\n", os.Args[1])
+	}
 }
